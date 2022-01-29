f1f7a67be1ddd5d6e75eca65ae2613c2766aaec0
Ryan Mesa <ryanmesa@gmail.com>
2022-01-29 18:48:05 -0500

    added deploy.sh and deploy.js and updated .travis.yml

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '44.202.81.171',
+      host: '54.174.214.171',
       username: 'ubuntu',
       privateKey: 'labsuser2.cer'
     })
